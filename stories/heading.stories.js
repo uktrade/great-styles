4ecd954d3f59f3f7147086d2697cf33600644dc4
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-07-24 11:10:33 +0100

    Add basic elements to storybook

--- stories/heading.stories.js
+++ stories/heading.stories.js
@@ -1,9 +1,13 @@
 import React from "react";
-import Heading from "../react-components/Heading";
 
 import "../styles/great-styles.css";
 
 export default { title: "Headings" };
 
-export const H1_HTML = () => <h1 className="h-xl text-great-blue">H1 HTML</h1>;
-export const H1_REACT = () => <Heading>H1 React</Heading>;
+export const Headings = () => (
+  <div className="m-s">
+    <h1 className="h-xl">.h-xl</h1>
+    <h1 className="h-l">.h-l</h1>
+    <h1 className="h-m">.h-m</h1>
+  </div>
+);
