419c1d19f25234d245f7b2effd0ebf159f323d9e
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-07-31 13:21:33 +0100

    Add large checkbox

--- stories/form.stories.js
+++ stories/form.stories.js
@@ -10,5 +10,10 @@ export const Form = () => (
       <input type="checkbox" id="checkbox" />
       <label htmlFor="checkbox">Label text</label>
     </div>
+    <p></p>
+    <div class="great-checkbox great-checkbox--large bg-black">
+      <input type="checkbox" id="checkbox_large" />
+      <label htmlFor="checkbox_large">Label text</label>
+    </div>
   </div>
 );
