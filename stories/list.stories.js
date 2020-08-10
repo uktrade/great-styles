093f18746adb4c22076f616b35a177b41cb608c2
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-08-10 09:38:36 +0100

    Fix typos

--- stories/list.stories.js
+++ stories/list.stories.js
@@ -7,9 +7,9 @@ export default { title: "List" };
 export const DotList = () => (
   <div className="m-s">
     <ul className="list-dot text-blue-deep-80">
-      <li>listem item 1</li>
-      <li>listem item 2</li>
-      <li>listem item 3</li>
+      <li>list item 1</li>
+      <li>list item 2</li>
+      <li>list item 3</li>
     </ul>
   </div>
 );
