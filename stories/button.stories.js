bb2bbcb2d5b85759e52d2465882508069afd967e
Gerald Isabelle <info@geraldisabelle.me>
2020-08-26 10:37:19 +0100

    add buttons with icons

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default { title: "Button" };
+export default { title: "Buttons" };
 
 export const Button = () => (
   <div className="m-s">
@@ -20,6 +20,24 @@ export const Button = () => (
     <p></p>
     <button className="button button--primary button--round-corner">
       .button .button--primary .button--round-corner
+    </button>
+    <br className='m-v-s'/>
+    <button className="button button--primary width-full">
+      .button .button--primary .width-full
+    </button>
+  </div>
+);
+
+export const Button_with_icon = () => (
+  <div className="m-s">
+    <button className="button button--secondary button--icon button--icon-left width-full">
+      <i className="fas fa-download" />
+      <span>.button .button--secondary .button--icon .button--icon-left</span>
+    </button>
+    <br className='m-v-s'/>
+    <button className="button button--secondary button--icon width-full">
+      <i className="fas fa-download" />
+      .button .button--secondary .button--icon
     </button>
   </div>
 );
