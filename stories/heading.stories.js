a34292b8f26d55176de335663026397ba24940cb
Bob Meredith <bob.meredith@ntlworld.com>
2020-09-03 13:11:11 +0100

    Corrected story classes

--- stories/heading.stories.js
+++ stories/heading.stories.js
@@ -15,10 +15,10 @@ export const Body = () => (
     <h1 className="body-xl">.body-xl</h1>
     <h1 className="body-l">.body-l</h1>
     <h1 className="body-m">.body-m</h1>
-    <h1 className="body-s">.body-small</h1>
+    <h1 className="body-s">.body-s</h1>
     <h1 className="body-xl-b">.body-xl-b</h1>
     <h1 className="body-l-b">.body-l-b</h1>
     <h1 className="body-m-b">.body-m-b</h1>
-    <h1 className="body-s-b">.body-small-b</h1>
+    <h1 className="body-s-b">.body-s-b</h1>
   </div>
 );
