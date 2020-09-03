9d3052242a20cd6317495e58f764e6d2b33af351
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-09-03 11:46:57 +0100

    Pros and cons

--- stories/box.stories.js
+++ stories/box.stories.js
@@ -5,5 +5,7 @@ export default { title: "Box" };
 export const Box = () => (
   <div className="m-s">
     <div className="box">.box</div>
+    <p></p>
+    <div className="box box--no-pointer">.box .box--no-pointer</div>
   </div>
 );
