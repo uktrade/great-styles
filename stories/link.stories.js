b3baf5a3ef5a1a3536acaaaa7031379fe8147b8d
Gerald Isabelle <info@geraldisabelle.me>
2020-09-03 16:11:20 +0100

    use button css for links

--- stories/link.stories.js
+++ stories/link.stories.js
@@ -13,3 +13,12 @@ export const Links = () => (
     </a>
   </div>
 );
+
+export const Links_as_button = () => (
+  <div className="m-s">
+    <a href='#' className="button button--secondary button--icon button--icon-left">
+      <i className="fab fa-linkedin" />
+      <span>.button .button--secondary .button--icon .button--icon-left</span>
+    </a>
+  </div>
+);
