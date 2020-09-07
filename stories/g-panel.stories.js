4207e5de5aafb98db3139c9feed798bdff8c9f4a
Gerald Isabelle <info@geraldisabelle.me>
2020-09-07 13:30:14 +0100

    add beta badge and style gpanel to match design

---
+++ stories/g-panel.stories.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default { title: 'G panel' };
+
+export const GPanel = () => (
+  <p className='g-panel'>For any questions, queries or feedback please get in touch with private@digital.trade.gov.uk</p>
+)
