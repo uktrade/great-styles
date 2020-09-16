2ae334fd12650c95434bb613f5b4646836a43800
Gerald Isabelle <info@geraldisabelle.me>
2020-09-16 16:28:45 +0100

    add missing colors

--- stories/colour.stories.js
+++ stories/colour.stories.js
@@ -2,63 +2,62 @@ import React from "react";
 
 export default { title: "Colours" };
 
+const colors = [
+  'black-100',
+  'black-90',
+  'black-80',
+  'black-70',
+  'black-60',
+  'black-50',
+  'black-40',
+  'black-30',
+  'black-20',
+  'black-10',
+  'blue-deep-100',
+  'blue-deep-90',
+  'blue-deep-80',
+  'blue-deep-70',
+  'blue-deep-60',
+  'blue-deep-50',
+  'blue-deep-40',
+  'blue-deep-30',
+  'blue-deep-20',
+  'blue-deep-10',
+  'red-100',
+  'red-90',
+  'red-80',
+  'red-70',
+  'red-60',
+  'red-50',
+  'red-40',
+  'red-30',
+  'red-20',
+  'red-10',
+  'green-100',
+  'green-30',
+  'light-grey',
+  'grey',
+  'white'
+]
+
 export const Text_Colours = () => (
   <div className="m-s">
-    <p className="text-black-100">.text-black-100</p>
-    <p className="text-black-90">.text-black-90</p>
-    <p className="text-black-80">.text-black-80</p>
-    <p className="text-black-70">.text-black-70</p>
-    <p className="text-black-60">.text-black-60</p>
-    <p className="text-black-50">.text-black-50</p>
-    <p className="text-black-40">.text-black-40</p>
-    <p className="text-black-30">.text-black-30</p>
-    <p className="text-black-20">.text-black-20</p>
-    <p className="text-black-10">.text-black-10</p>
-    <p className="text-black-05">.text-black-05</p>
-    <p className="text-blue-deep-80">.text-blue-deep-80</p>
-    <p className="text-blue-deep-60">.text-blue-deep-60</p>
-    <p className="text-blue-deep-40">.text-blue-deep-40</p>
-    <p className="text-blue-deep-20">.text-blue-deep-20</p>
-    <p className="text-blue-deep-10">.text-blue-deep-10</p>
-    <p className="text-aqua-100">.text-aqua-100</p>
-    <p className="text-red-80">.text-red-80</p>
-    <p className="text-white" style={{ background: "black" }}>
-      .text-white
-    </p>
-    <p className="text-green-100">.text-green-100</p>
-    <p className="text-green-30">.text-green-30</p>
-    <p className="text-grey">.text-grey</p>
-    <p className="text-light-grey">.text-light-grey</p>
+    {colors.map(color => {
+      const textColor = `text-${color}`
+      return (
+        <p className={`${textColor} ${color === 'white' && 'bg-black-100'} `}>{`.${textColor}`}</p>
+      )
+    })}
   </div>
 );
 
 export const BG_Colours = () => (
   <div className="m-s" style={{ color: "white" }}>
-    <p className="bg-black-100">.bg-black-100</p>
-    <p className="bg-black-90">.bg-black-90</p>
-    <p className="bg-black-80">.bg-black-80</p>
-    <p className="bg-black-70">.bg-black-70</p>
-    <p className="bg-black-60">.bg-black-60</p>
-    <p className="bg-black-50">.bg-black-50</p>
-    <p className="bg-black-40">.bg-black-40</p>
-    <p className="bg-black-30">.bg-black-30</p>
-    <p className="bg-black-20">.bg-black-20</p>
-    <p className="bg-black-10">.bg-black-10</p>
-    <p className="bg-black-05">.bg-black-05</p>
-    <p className="bg-blue-deep-100">.bg-blue-deep-100</p>
-    <p className="bg-blue-deep-80">.bg-blue-deep-80</p>
-    <p className="bg-blue-deep-60">.bg-blue-deep-60</p>
-    <p className="bg-blue-deep-40">.bg-blue-deep-40</p>
-    <p className="bg-blue-deep-20">.bg-blue-deep-20</p>
-    <p className="bg-blue-deep-10">.bg-blue-deep-10</p>
-    <p className="bg-aqua-100">.bg-aqua-100</p>
-    <p className="bg-red-80">.bg-red-80</p>
-    <p className="bg-white" style={{ color: "black" }}>
-      .bg-white
-    </p>
-    <p className="bg-green-100">.bg-green-100</p>
-    <p className="bg-green-30">.bg-green-30</p>
-    <p className="bg-grey">.bg-grey</p>
-    <p className="bg-light-grey">.bg-light-grey</p>
+    {colors.map(color => {
+      const backgroundColor = `bg-${color}`
+      return (
+        <p className={`${backgroundColor} ${color === 'white' && 'text-black-100'} `}>{`.${backgroundColor}`}</p>
+      )
+    })}
   </div>
 );
