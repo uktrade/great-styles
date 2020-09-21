49d5429f65e9342035d0eeee7f53403328790fc9
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-09-21 15:09:14 +0100

    Choose dont choose component

--- stories/media-block.stories.js
+++ stories/media-block.stories.js
@@ -18,5 +18,13 @@ export const MediaBlock = () => (
         <p className="text-blue-deep-80">Paragraph text</p>
       </div>
     </div>
+    <p></p>
+    <div className="media-block media-block--round-image m-b-s m-h-s">
+      <i className="fas fa-ban"></i>
+      <div className="g-panel g-panel--horizontal-mobile">
+        <h3 className="h-l text-blue-deep-80">Heading text</h3>
+        <p className="text-blue-deep-80">Paragraph text</p>
+      </div>
+    </div>
   </div>
 );
