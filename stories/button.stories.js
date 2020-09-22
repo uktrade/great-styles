d8493a8f35f7878484394de32730b03417ba8bbf
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-09-22 16:37:08 +0100

    Primary, secondary and tertiary

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -2,29 +2,29 @@ import React from "react";
 
 export default { title: "Buttons" };
 
+const buttons = [
+  "button button--large",
+  "button",
+  "button button--small",
+  "button button--secondary button--large",
+  "button button--secondary",
+  "button button--secondary button--small",
+  "button button--tertiary button--large",
+  "button button--tertiary",
+  "button button--tertiary button--small"
+];
+
 export const Button = () => (
-  <div className="m-s">
-    <button className="button button--primary">.button .button--primary</button>
-    <p></p>
-    <button className="button button--primary button--large">
-      .button .button--primary .button--large
-    </button>
-    <p></p>
-    <button className="button button--secondary">
-      .button .button--secondary
-    </button>
-    <p></p>
-    <button className="button button--tertiary">
-      .button .button--tertiary
-    </button>
-    <p></p>
-    <button className="button button--primary button--round-corner">
-      .button .button--primary .button--round-corner
-    </button>
-    <br className='m-v-s'/>
-    <button className="button button--primary width-full">
-      .button .button--primary .width-full
-    </button>
+  <div>
+    {buttons.map(classes => {
+      return (
+        <div className="m-b-s">
+          <button className={classes}>
+            {classes.split().map(c => `.${c}`)}
+          </button>
+        </div>
+      );
+    })}
   </div>
 );
 
@@ -34,7 +34,7 @@ export const Button_with_icon = () => (
       <i className="fas fa-download" />
       <span>.button .button--secondary .button--icon .button--icon-left</span>
     </button>
-    <br className='m-v-s'/>
+    <br className="m-v-s" />
     <button className="button button--secondary button--icon width-full">
       <i className="fas fa-download" />
       .button .button--secondary .button--icon
@@ -44,17 +44,25 @@ export const Button_with_icon = () => (
 
 export const Button_disabled = () => (
   <div className="m-s">
-    <button className="button button--primary" disabled>.button .button--primary</button>
-    <br className='m-v-s'/>
-    <button className="button button--secondary" disabled>.button .button--secondary</button>
-    <br className='m-v-s'/>
-    <button className="button button--tertiary" disabled>.button .button--tertiary</button>
-    <br className='m-v-s'/>
+    <button className="button button--primary" disabled>
+      .button .button--primary
+    </button>
+    <br className="m-v-s" />
+    <button className="button button--secondary" disabled>
+      .button .button--secondary
+    </button>
+    <br className="m-v-s" />
+    <button className="button button--tertiary" disabled>
+      .button .button--tertiary
+    </button>
+    <br className="m-v-s" />
   </div>
 );
 
 export const Button_as_icon = () => (
   <div className="m-s">
-    <button type='button' className='button button--icon-center'><i className='fas fa-share text-blue-deep-60 icon--border'/></button>
+    <button type="button" className="button button--icon-center">
+      <i className="fas fa-share text-blue-deep-60 icon--border" />
+    </button>
   </div>
-)
+);
