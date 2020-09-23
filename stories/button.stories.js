b645ffa9c8d3d5eb728e390f7a076a0f6df78d5a
Lewis Coulson <lewis.coulson@digital.trade.gov.uk>
2020-09-23 15:33:54 +0100

    First stab at all button variants

--- stories/button.stories.js
+++ stories/button.stories.js
@@ -2,67 +2,99 @@ import React from "react";
 
 export default { title: "Buttons" };
 
-const buttons = [
-  "button button--large",
-  "button",
-  "button button--small",
-  "button button--secondary button--large",
-  "button button--secondary",
-  "button button--secondary button--small",
-  "button button--tertiary button--large",
-  "button button--tertiary",
-  "button button--tertiary button--small"
-];
+const renderButtons = (buttons, isDisabled = false, isIconOnly = false) => {
+  return (
+    <div>
+      {buttons.map(classes => {
+        return (
+          <div className="m-b-s">
+            <button className={classes} disabled={isDisabled}>
+              {classes.split().map(c => {
+                if (c.includes("loading")) {
+                  return <div class="button--loading__spinner">Loading...</div>;
+                } else if (c.includes("icon")) {
+                  return (
+                    <>
+                      <i className="fas fa-plus" />
+                      {!isIconOnly && <span>{`.${c}`}</span>}
+                    </>
+                  );
+                } else {
+                  return `.${c}`;
+                }
+              })}
+            </button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export const Button = () => (
-  <div>
-    {buttons.map(classes => {
-      return (
-        <div className="m-b-s">
-          <button className={classes}>
-            {classes.split().map(c => `.${c}`)}
-          </button>
-        </div>
-      );
-    })}
-  </div>
-);
+export const button_large = () =>
+  renderButtons([
+    "button button--large",
+    "button button--large button--secondary ",
+    "button button--large button--tertiary "
+  ]);
 
-export const Button_with_icon = () => (
-  <div className="m-s">
-    <button className="button button--secondary button--icon button--icon-left width-full">
-      <i className="fas fa-download" />
-      <span>.button .button--secondary .button--icon .button--icon-left</span>
-    </button>
-    <br className="m-v-s" />
-    <button className="button button--secondary button--icon width-full">
-      <i className="fas fa-download" />
-      .button .button--secondary .button--icon
-    </button>
-  </div>
-);
+export const button_medium = () =>
+  renderButtons([
+    "button",
+    "button button--secondary ",
+    "button button--tertiary "
+  ]);
 
-export const Button_disabled = () => (
-  <div className="m-s">
-    <button className="button button--primary" disabled>
-      .button .button--primary
-    </button>
-    <br className="m-v-s" />
-    <button className="button button--secondary" disabled>
-      .button .button--secondary
-    </button>
-    <br className="m-v-s" />
-    <button className="button button--tertiary" disabled>
-      .button .button--tertiary
-    </button>
-    <br className="m-v-s" />
-  </div>
-);
+export const button_small = () =>
+  renderButtons([
+    "button button--small",
+    "button button--small button--secondary ",
+    "button button--small button--tertiary "
+  ]);
 
-export const Button_as_icon = () => (
-  <div className="m-s">
-    <button type="button" className="button button--icon-center">
-      <i className="fas fa-share text-blue-deep-60 icon--border" />
-    </button>
-  </div>
-);
+export const button_large_icon = () =>
+  renderButtons([
+    "button button--large button--icon",
+    "button button--large button--secondary button--icon",
+    "button button--large button--tertiary button--icon"
+  ]);
+
+export const button_medium_icon = () =>
+  renderButtons([
+    "button button--icon",
+    "button button--secondary button--icon",
+    "button button--tertiary button--icon"
+  ]);
+
+export const button_disabled = () =>
+  renderButtons(
+    ["button", "button button--secondary ", "button button--tertiary "],
+    true
+  );
+
+export const button_loading = () =>
+  renderButtons([
+    "button button--small button--loading",
+    "button button--small button--loading button--secondary",
+    "button button--small button--loading button--tertiary",
+    "button button--loading",
+    "button button--loading button--secondary",
+    "button button--loading button--tertiary",
+    "button button--large button--loading",
+    "button button--large button--loading button--secondary",
+    "button button--large button--loading button--tertiary"
+  ]);
+
+export const button_small_only_icon = () =>
+  renderButtons(
+    [
+      "button button--small button--only-icon",
+      "button button--small button--only-icon button--secondary",
+      "button button--small button--only-icon button--tertiary",
+      "button button--small button--only-icon button--loading",
+      "button button--small button--only-icon button--loading button--secondary",
+      "button button--small button--only-icon button--loading button--tertiary"
+    ],
+    false,
+    true
+  );
