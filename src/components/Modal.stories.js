47d637565ed99222a22e50214917d52dca9f7f1a
Jake Hopking <jake@hopking.io>
2020-12-15 13:53:26 +0000

    Organise instances

--- src/components/Modal.stories.js
+++ src/components/Modal.stories.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default {title: "Overlays"};
 
-export const Modal_window = () => (
+export const Modal_window_1 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header radius-top-md bg-blue-deep-80 p-s">
+      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-md bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
         </a>
@@ -26,48 +26,6 @@ export const Modal_window = () => (
 export const Modal_window_2 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-md bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
-        </a>
-      </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
-        <div className="p-s">
-          <h4 className="h-xs p-0">Add your target market</h4>
-          <p className="m-t-xs">
-            You will need to choose a target market before you can complete this section
-          </p>
-          <button className="button">Add a target market</button>
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
-export const Modal_window_3 = () => (
-  <div className="bg-blue-deep-10 modal-container">
-    <div className="modal">
-      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-md bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
-        </a>
-      </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
-        <div className="p-s">
-          <h4 className="h-xs p-0">Add your target market</h4>
-          <p className="m-t-xs">
-            You will need to choose a target market before you can complete this section
-          </p>
-          <button className="button">Add a target market</button>
-        </div>
-      </div>
-    </div>
-  </div>
-);
-
-export const Modal_window_4 = () => (
-  <div className="bg-blue-deep-10 modal-container">
-    <div className="modal">
       <div className="modal-header modal-header-bg modal-header-bg--2 radius-top-md bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
@@ -86,7 +44,7 @@ export const Modal_window_4 = () => (
   </div>
 );
 
-export const Modal_window_5 = () => (
+export const Modal_window_3 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
       <div className="modal-header modal-header-bg modal-header-bg--3 radius-top-md bg-blue-deep-80 p-s">
@@ -102,6 +60,27 @@ export const Modal_window_5 = () => (
             platform.
           </p>
           <button className="button">OK</button>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export const Modal_window_no_image = () => (
+  <div className="bg-blue-deep-10 modal-container">
+    <div className="modal">
+      <div className="modal-header radius-top-md bg-blue-deep-80 p-s">
+        <a href="/test" className="link text-white body-m modal-close">
+          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
+        </a>
+      </div>
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
+        <div className="p-s">
+          <h4 className="h-xs p-0">Add your target market</h4>
+          <p className="m-t-xs">
+            You will need to choose a target market before you can complete this section
+          </p>
+          <button className="button">Add a target market</button>
         </div>
       </div>
     </div>
