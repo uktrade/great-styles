fa6cc73931efce7c3261619e015ca541eee337d9
Jake Hopking <jake@hopking.io>
2020-12-17 11:53:13 +0000

    Rename responsive util

--- src/components/Modal.stories.js
+++ src/components/Modal.stories.js
@@ -1,112 +1,118 @@
-import React from "react";
+import React from 'react'
 
-export default {title: "Overlays"};
+export default { title: 'Overlays' }
 
 export const Modal_window_1 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-md bg-blue-deep-80 p-s">
+      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-s bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
         </a>
       </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
         <div className="p-s">
           <h4 className="h-xs p-0">Add your target market</h4>
           <p className="m-t-xs">
-            You will need to choose a target market before you can complete this section
+            You will need to choose a target market before you can complete this
+            section
           </p>
           <button className="button">Add a target market</button>
         </div>
       </div>
     </div>
   </div>
-);
+)
 
 export const Modal_window_2 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header modal-header-bg modal-header-bg--2 radius-top-md bg-blue-deep-80 p-s">
+      <div className="modal-header modal-header-bg modal-header-bg--2 radius-top-s bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
         </a>
       </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
         <div className="p-s">
           <h4 className="h-xs p-0">Add your product</h4>
           <p className="m-t-xs">
-            You will need to choose a product before you can complete this section
+            You will need to choose a product before you can complete this
+            section
           </p>
           <button className="button">Add a product</button>
         </div>
       </div>
     </div>
   </div>
-);
+)
 
 export const Modal_window_3 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header modal-header-bg modal-header-bg--3 radius-top-md bg-blue-deep-80 p-s">
+      <div className="modal-header modal-header-bg modal-header-bg--3 radius-top-s bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
         </a>
       </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
         <div className="p-s">
           <h4 className="h-xs p-0">This feature is coming soon</h4>
           <p className="m-t-xs">
-            This feature is not available in the Beta version of the new great.gov.uk
-            platform.
+            This feature is not available in the Beta version of the new
+            great.gov.uk platform.
           </p>
           <button className="button">OK</button>
         </div>
       </div>
     </div>
   </div>
-);
+)
 
 export const Modal_window_no_image = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header radius-top-md bg-blue-deep-80 p-s">
+      <div className="modal-header radius-top-s bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
         </a>
       </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
         <div className="p-s">
           <h4 className="h-xs p-0">Add your target market</h4>
           <p className="m-t-xs">
-            You will need to choose a target market before you can complete this section
+            You will need to choose a target market before you can complete this
+            section
           </p>
           <button className="button">Add a target market</button>
         </div>
       </div>
     </div>
   </div>
-);
+)
 
 export const Modal_window_with_footer = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
-      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-md bg-blue-deep-80 p-s">
+      <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-s bg-blue-deep-80 p-s">
         <a href="/test" className="link text-white body-m modal-close">
           <i className="fas fa-arrow-circle-left m-r-xxs" /> back
         </a>
       </div>
-      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-md">
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
         <div className="p-s">
           <h4 className="h-xs p-0">Add your target market</h4>
           <p className="m-t-xs">
-            You will need to choose a target market before you can complete this section
+            You will need to choose a target market before you can complete this
+            section
           </p>
           <button className="button">Add a target market</button>
         </div>
         <div className="modal-footer">
           <hr className="hr hr--light m-0" />
           <div className="p-h-s p-b-s">
-            <h4 className="h-xs p-b-xs">Select a market you’ve already researched</h4>
+            <h4 className="h-xs p-b-xs">
+              Select a market you’ve already researched
+            </h4>
             <button className="tag tag--icon tag--tertiary inline m-r-xs">
               United Kingdom
               <i className="fas fa-plus" />
@@ -120,4 +126,4 @@ export const Modal_window_with_footer = () => (
       </div>
     </div>
   </div>
-);
+)
