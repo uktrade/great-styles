1256d59ae3845580534d9f5a6322a7425e9059cf
Jake Hopking <jake@hopking.io>
2020-12-22 17:12:49 +0000

    Fixes

--- src/components/tooltip/Tooltip.jsx
+++ src/components/tooltip/Tooltip.jsx
@@ -18,6 +18,8 @@ export const Tooltip = ({
     isVisible
   )
   const [tooltipPosition, setTooltipPosition] = useState(null)
+  const [mount, setMount] = useState(false)
+  const [tooltipOpen, setTooltipOpen] = useState(false)
   // Find mobile breakpoint width from CSS var
   const mobileBreakpoint =
     Number(
@@ -39,8 +41,10 @@ export const Tooltip = ({
 
   const onClickOpen = () => {
     setIsComponentVisible(true)
-    // Provide time for element to render in DOM
-    if (window.innerWidth <= mobileBreakpoint) {
+    setTooltipOpen(true)
+
+    if (window.innerWidth <= mobileBreakpoint && !tooltipOpen) {
+      // Provide time for element to render in DOM
       setTimeout(() => {
         updatePositionOffset(ref)
       }, 50)
@@ -50,18 +54,23 @@ export const Tooltip = ({
   const onClickClose = () => {
     setIsComponentVisible(false)
     updatePositionOffset(ref)
+    setTooltipOpen(false)
   }
 
-  useEffect(() => {
-    if (isComponentVisible) {
-      updatePositionOffset(ref)
-      window.addEventListener('resize', updatePositionOffset(ref))
-      return () => {
-        window.removeEventListener('resize', updatePositionOffset(ref))
+  useEffect(
+    () => {
+      if (isComponentVisible && !mount) {
+        updatePositionOffset(ref)
+        setMount(true)
+        window.addEventListener('resize', updatePositionOffset(ref))
+        return () => {
+          window.removeEventListener('resize', updatePositionOffset(ref))
+        }
       }
-    }
+    },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [window.innerWidth])
+    [isComponentVisible, ref, mount]
+  )
 
   // Logic for left or right aligned. Default left.
   const ttPosition = position === 'right' ? 'right' : 'left'
@@ -71,35 +80,34 @@ export const Tooltip = ({
       <div title="Click to view Educational moment" className="tooltip__icon">
         <a
           className="button button--small button--only-icon button--tertiary"
-          onClick={() => onClickOpen()}
+          onClick={onClickOpen}
           role="button"
           tabIndex="0"
+          type="button"
         >
           <i className={`fas ${faIcon}`} />
         </a>
       </div>
       {isComponentVisible && (
-        <>
-          <div
-            ref={ref}
-            className={`tooltip__text tooltip__text--${ttPosition} bg-white radius radius--small`}
-            style={tooltipPosition}
+        <div
+          ref={ref}
+          className={`tooltip__text tooltip__text--${ttPosition} bg-white radius radius--small`}
+          style={tooltipPosition}
+        >
+          <span
+            className="tooltip__close"
+            title="Click or press Escape to close Educational moment"
+            onClick={onClickClose}
           >
-            <span
-              className="tooltip__close"
-              title="Click or press Escape to close Educational moment"
-              onClick={() => onClickClose()}
-            >
-              <i className="fas fa-times text-blue-deep-80" />
-            </span>
-            {title && (
-              <div className="tooltip__title h-xs p-t-0 p-b-0">{title}</div>
-            )}
-            <div className="tooltip__content text-blue-deep-80">
-              {ReactHtmlParser(content)}
-            </div>
+            <i className="fas fa-times text-blue-deep-80" />
+          </span>
+          {title && (
+            <div className="tooltip__title h-xs p-t-0 p-b-0">{title}</div>
+          )}
+          <div className="tooltip__content text-blue-deep-80">
+            {ReactHtmlParser(content)}
           </div>
-        </>
+        </div>
       )}
     </div>
   )
