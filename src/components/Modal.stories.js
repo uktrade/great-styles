03962222ad57b63e0207b504bc575301b01a74ef
Jake Hopking <jake@hopking.io>
2021-01-12 13:25:28 +0000

    Add and update new modal window examples from design.

--- src/components/Modal.stories.js
+++ src/components/Modal.stories.js
@@ -6,8 +6,17 @@ export const Modal_window_1 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
       <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-s bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
+        <a
+          href="/"
+          className="link text-white body-m modal-close modal-close--back"
+        >
+          <i className="fas fa-arrow-left m-r-xxs" /> Back
+        </a>
+        <a
+          href="/"
+          className="link text-white modal-close modal-close--x modal-close--x-large"
+        >
+          <i className="fas fa-times-circle" />
         </a>
       </div>
       <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
@@ -28,8 +37,8 @@ export const Modal_window_2 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
       <div className="modal-header modal-header-bg modal-header-bg--2 radius-top-s bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
+        <a href="/" className="link text-white body-m modal-close">
+          <i className="fas fa-arrow-left m-r-xxs" /> Back
         </a>
       </div>
       <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
@@ -50,8 +59,8 @@ export const Modal_window_3 = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
       <div className="modal-header modal-header-bg modal-header-bg--3 radius-top-s bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
+        <a href="/" className="link text-white body-m modal-close">
+          <i className="fas fa-arrow-left m-r-xxs" /> Back
         </a>
       </div>
       <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
@@ -68,12 +77,82 @@ export const Modal_window_3 = () => (
   </div>
 )
 
+export const Modal_window_4 = () => (
+  <div className="bg-blue-deep-10 modal-container">
+    <div className="modal">
+      <div className="modal-header bg-white radius-top-s p-h-s p-t-s">
+        <a
+          href="/"
+          className="button button--secondary button--small button--icon modal-close"
+        >
+          <i className="fas fa-arrow-left" />
+          <span>Back</span>
+        </a>
+      </div>
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
+        <div className="p-s">
+          <h4 className="h-xs p-0">This feature is coming soon</h4>
+          <p className="m-t-xs">
+            This feature is not available in the Beta version of the new
+            great.gov.uk platform.
+          </p>
+          <button className="button">OK</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export const Modal_window_5 = () => (
+  <div className="bg-blue-deep-10 modal-container">
+    <div className="modal">
+      <div className="modal-header bg-white radius-top-s p-h-s p-t-s">
+        <a href="/" className="link modal-close modal-close--x">
+          <i className="fas fa-times" />
+        </a>
+      </div>
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
+        <div className="p-h-s p-t-0 p-b-s">
+          <h4 className="h-xs p-0">Add your target market</h4>
+          <p className="m-t-xs">
+            You will need to choose a target market before you can complete this
+            section.
+          </p>
+          <button className="button">Add a target market</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export const Modal_window_6_empty = () => (
+  <div className="bg-blue-deep-10 modal-container">
+    <div className="modal">
+      <div className="modal-header bg-white radius-top-s p-h-s p-t-s">
+        <a
+          href="/"
+          className="button button--secondary button--small button--icon modal-close modal-close--back m-t-n-xs m-l-n-xs"
+        >
+          <i className="fas fa-arrow-left" />
+          <span>Back</span>
+        </a>
+        <a href="/" className="link modal-close modal-close--x">
+          <i className="fas fa-times" />
+        </a>
+      </div>
+      <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
+        <div className="p-h-s p-t-0 p-b-s" style={{ height: '200px' }}></div>
+      </div>
+    </div>
+  </div>
+)
+
 export const Modal_window_no_image = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
       <div className="modal-header radius-top-s bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
+        <a href="/" className="link text-white body-m modal-close">
+          <i className="fas fa-arrow-left m-r-xxs" /> Back
         </a>
       </div>
       <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
@@ -94,8 +173,8 @@ export const Modal_window_with_footer = () => (
   <div className="bg-blue-deep-10 modal-container">
     <div className="modal">
       <div className="modal-header modal-header-bg modal-header-bg--1 radius-top-s bg-blue-deep-80 p-s">
-        <a href="/test" className="link text-white body-m modal-close">
-          <i className="fas fa-arrow-circle-left m-r-xxs" /> back
+        <a href="/" className="link text-white body-m modal-close">
+          <i className="fas fa-arrow-circle-left m-r-xxs" /> Back
         </a>
       </div>
       <div className="modal-inner text-blue-deep-80 bg-white radius-bottom-s">
