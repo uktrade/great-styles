67c4c769200b725c3604e52f7418cc0bc4d48cf6
Jake Hopking <jake@hopking.io>
2021-04-19 12:35:21 +0100

    Add storybook knobs. Add body and children modal variants

--- src/components/NewModal.stories.js
+++ src/components/NewModal.stories.js
@@ -1,5 +1,14 @@
 import React from 'react'
-import { withKnobs, boolean, select, text } from '@storybook/addon-knobs'
+import {
+  withKnobs,
+  boolean,
+  select,
+  text,
+  number,
+  object,
+} from '@storybook/addon-knobs'
+import { Learn_linking } from './Card-learn-linking.stories'
+import { Quote } from './Quote.stories'
 import { Modal } from './modal/Modal'
 
 import modalImage from '../images/modal-3.png'
@@ -12,34 +21,68 @@ export default {
   },
 }
 
-// const CustomButton = () => {
-//   return (
-//     <button className="button button--small button--secondary" type="button">
-//       Open custom
-//     </button>
-//   )
-// }
-
-export const Modal_1 = () => {
+export const Modal_with_body = () => {
   return (
-    <Modal {...Modal_1.props}>
-      <p>Child content</p>
-    </Modal>
+    <Modal
+      {...Modal_with_body.props}
+      modalSize={select(
+        'Modal size',
+        { small: 'small', medium: 'medium', large: 'large' },
+        'medium'
+      )}
+      progressPercentage={number('Progress percentage', null)}
+      title={text('Modal title', 'Modal title')}
+      closeClick={boolean('Modal close', true)}
+      primaryButtonClick={boolean('Primary button enable', true)}
+      primaryButtonLabel={text('Primary button label', 'Done')}
+      primaryButtonDisable={boolean('Primary button disabled', false)}
+      secondaryButtonClick={boolean('Secondary button enable', true)}
+      secondaryButtonLabel={text('Secondary button label', 'Cancel')}
+      secondaryButtonDisable={boolean('Secondary button disabled', false)}
+      image={object('Modal image', {
+        src: modalImage,
+        alt: 'Two people carrying a massive pencil',
+        bgClass: 'bg-blue-deep-80',
+      })}
+    />
   )
 }
-Modal_1.props = {
-  title: 'Modal title',
-  body: `<p>Some content here Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut error distinctio laboriosam quae at quibusdam animi odit, deleniti nostrum iusto adipisci tenetur id rem eos, quos voluptatum accusantium beatae magni!</p>`,
-  image: {
-    src: modalImage,
-    alt: 'Alt text',
-    bgClass: 'bg-blue-deep-80',
-  },
-  primaryButtonClick: () => console.log('Primary clicked'),
-  primaryButtonLabel: 'Done',
-  secondaryButtonClick: () => console.log('Secondary clicked'),
-  secondaryButtonLabel: 'Cancel',
-  closeClick: () => console.log('Close clicked'),
-  progressPercentage: 50,
-  // modalSize: 'small',
+Modal_with_body.props = {
+  body: `
+    <p>Some content here Lorem ipsum dolor sit amet Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus veritatis numquam nobis suscipit doloremque sit cupiditate fugiat:</p>
+    <ul>
+      <li>List item</li>
+      <li>List item</li>
+      <li>List item</li>
+    </ul>
+  `,
+  //   primaryButtonClick: () => console.log('Primary clicked'),
+  //   secondaryButtonClick: () => console.log('Secondary clicked'),
+  //   closeClick: () => console.log('Close clicked'),
+}
+
+export const Modal_with_children = () => {
+  return (
+    <Modal
+      modalSize={select(
+        'Modal size',
+        { small: 'small', medium: 'medium', large: 'large' },
+        'medium'
+      )}
+      progressPercentage={number('Progress percentage', null)}
+      title={text('Modal title', '')}
+      closeClick={boolean('Modal close', true)}
+      primaryButtonClick={boolean('Primary button enable', true)}
+      primaryButtonLabel={text('Primary button label', 'Done')}
+      primaryButtonDisable={boolean('Primary button disabled', false)}
+      secondaryButtonClick={boolean('Secondary button enable', false)}
+      secondaryButtonLabel={text('Secondary button label', 'Cancel')}
+      secondaryButtonDisable={boolean('Secondary button disabled', false)}
+    >
+      <>
+        <Learn_linking />
+        <Quote />
+      </>
+    </Modal>
+  )
 }
