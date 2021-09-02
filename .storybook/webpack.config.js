cf0a151ac4b72e5845fad962d8abe940a64c231e
Le H Ngo <hgle.ngo@gmail.com>
2021-09-02 21:14:55 +0100

    - Adds absolute path to webpack config to fix import errors while building.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,4 +1,11 @@
+const path = require('path')
+
 module.exports = {
+  resolve: {
+    alias: {
+      helpers: path.resolve(__dirname, '../src/scss/helpers/'),
+    },
+  },
   module: {
     rules: [
       {
