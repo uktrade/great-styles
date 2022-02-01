d4dbdd46dcf4a3cddc22ca72fe5bbfa467a3b16d
Fred Marecesche <fred@froddd.co.uk>
2022-02-01 14:13:14 +0000

    Prevent tooltip close button text overlap

--- dist/components/tooltip/Tooltip.js
+++ dist/components/tooltip/Tooltip.js
@@ -1,7 +1,5 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
-
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
@@ -17,65 +15,51 @@ var _useComponentVisible = _interopRequireDefault(require("../../hooks/useCompon
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
 
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
-function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
-
-function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-function _iterableToArrayLimit(arr, i) { var _i = arr == null ? null : typeof Symbol !== "undefined" && arr[Symbol.iterator] || arr["@@iterator"]; if (_i == null) return; var _arr = []; var _n = true; var _d = false; var _s, _e; try { for (_i = _i.call(arr); !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-
-function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-var Tooltip = function Tooltip(_ref) {
-  var title = _ref.title,
-      content = _ref.content,
-      position = _ref.position,
-      className = _ref.className,
-      isVisible = _ref.isVisible,
-      faIcon = _ref.faIcon,
-      showTitle = _ref.showTitle;
+const Tooltip = _ref => {
+  let {
+    title,
+    content,
+    position,
+    className,
+    isVisible,
+    faIcon,
+    showTitle
+  } = _ref;
   // Init useComponentVisible hook
-  var componentVisible = _useComponentVisible.default;
+  const componentVisible = _useComponentVisible.default;
+  const {
+    ref,
+    isComponentVisible,
+    setIsComponentVisible
+  } = componentVisible(isVisible);
+  const [mount, setMount] = (0, _react.useState)(false);
 
-  var _componentVisible = componentVisible(isVisible),
-      ref = _componentVisible.ref,
-      isComponentVisible = _componentVisible.isComponentVisible,
-      setIsComponentVisible = _componentVisible.setIsComponentVisible;
-
-  var _useState = (0, _react.useState)(false),
-      _useState2 = _slicedToArray(_useState, 2),
-      mount = _useState2[0],
-      setMount = _useState2[1];
-
-  var onClickOpen = function onClickOpen() {
+  const onClickOpen = () => {
     setIsComponentVisible(true);
   };
 
-  var onClickClose = function onClickClose() {
+  const onClickClose = () => {
     setIsComponentVisible(false);
   };
 
-  var onKeyClose = function onKeyClose(key) {
+  const onKeyClose = key => {
     if (key === 'Enter') {
       onClickClose();
     }
   };
 
-  (0, _react.useEffect)(function () {
+  (0, _react.useEffect)(() => {
     if (isComponentVisible && !mount) {
       setMount(true);
     }
   }, // eslint-disable-next-line react-hooks/exhaustive-deps
   [isComponentVisible, ref, mount]); // Logic for left or right aligned. Default left.
 
-  var ttPosition = position === 'right' ? 'right' : 'left';
+  const ttPosition = position === 'right' ? 'right' : 'left';
   return /*#__PURE__*/_react.default.createElement("div", {
     className: "tooltip ".concat(className)
   }, /*#__PURE__*/_react.default.createElement("div", {
@@ -99,9 +83,7 @@ var Tooltip = function Tooltip(_ref) {
     className: "tooltip__close",
     title: "Click or press Escape to close Educational moment",
     onClick: onClickClose,
-    onKeyUp: function onKeyUp(e) {
-      return onKeyClose(e.key);
-    },
+    onKeyUp: e => onKeyClose(e.key),
     tabIndex: "0"
   }, /*#__PURE__*/_react.default.createElement("i", {
     className: "fas fa-times text-blue-deep-80"
