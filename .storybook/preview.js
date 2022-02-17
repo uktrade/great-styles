9ea110f0a86ae0ddaa1a5ffb5778129624866f72
Fred Marecesche <fred@froddd.co.uk>
2022-02-17 11:21:56 +0000

    Consolidate base styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,12 +1,13 @@
-import "../src/scss/great-styles.scss";
+import '../src/scss/great-styles.scss'
 
 export const parameters = {
   layout: 'centered',
-  docs: {page: null},
+  docs: { page: null },
   // TODO: Define some actual organisation for all components/atoms/modules
   // Sort the stories and folders alphabetically (at the top level)
   options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, {numeric: true}),
+    storySort: {
+      order: ['Base styles', ['Colours', 'Typography', 'Grid'], 'Elements'],
+    },
   },
-};
+}
