a2f7ce67a7f0e80f4fb3ccd5f5154cfa859facc5
Fred Marecesche <fred@froddd.co.uk>
2022-02-17 14:19:44 +0000

    Tidy up lists

--- src/stories/elements/List.stories.js
+++ src/stories/elements/List.stories.js
@@ -3,17 +3,33 @@ import React from 'react'
 export default { title: 'Elements/Lists' }
 
 export const Unordered = () => (
-  <div className='m-s'>
-    <ul className='list-dot text-blue-deep-80'>
+  <div className='body-l'>
+    <ul className='list-bullet'>
+      <li>list item 1</li>
+      <li>list item 2</li>
+      <li>list item 3</li>
+    </ul>
+    <ul className='list-dot'>
       <li>list item 1</li>
       <li>list item 2</li>
       <li>list item 3</li>
     </ul>
   </div>
 )
+
 export const Ordered = () => (
-  <div className='m-s'>
-    <ol className='list-circle-number bg-blue-deep-80'>
+  <div className='body-l'>
+    <ol className='list-number'>
+      <li>list item 1</li>
+      <li>list item 2</li>
+      <li>list item 3</li>
+    </ol>
+  </div>
+)
+
+export const OrderedLargeNumbers = () => (
+  <div className='bg-blue-deep-80 m-s p-m'>
+    <ol className='list-circle-number'>
       <li>
         <span>
           Which ways of funding which might be right for your company’s stage of growth
