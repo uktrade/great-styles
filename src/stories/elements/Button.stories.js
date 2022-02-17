c09f6bbad862bb1de9a5fc48f37992081bb15ea7
Fred Marecesche <fred@froddd.co.uk>
2022-02-17 14:07:28 +0000

    Tidy up buttons and tags

--- src/stories/elements/Button.stories.js
+++ src/stories/elements/Button.stories.js
@@ -3,175 +3,72 @@ import { select, text } from '@storybook/addon-knobs'
 
 export default { title: 'Elements/Button' }
 
-const renderButtons = (buttons, isDisabled = false, isIconOnly = false) => {
-  const label = text('Button text', '')
-  const icon = buttons.some(b => b.includes('-icon')) ? select('Button icon', ['plus', 'plus-square', 'download', 'arrow-right', 'arrow-left', 'chevron-down', 'chevron-up'], 'plus') : null
+const buttons = [
+  { label: 'Primary', classes: 'button' },
+  { label: 'Secondary', classes: 'button button--secondary' },
+  { label: 'Tertiary', classes: 'button button--tertiary' },
+  { label: 'Quaternary', classes: 'button button--quaternary' },
+]
+
+const renderButtons = (extra_classes = '', isDisabled = false) => {
+  const icon = extra_classes.includes('-icon') ? select('Button icon', ['plus', 'plus-square', 'download', 'arrow-right', 'arrow-left', 'chevron-down', 'chevron-up'], 'plus') : null
+  const isLoading = extra_classes.includes('button--loading')
+  const isIconOnly = isLoading || extra_classes.includes('--only-icon')
+
   return (
-    <div>
-      {buttons.map((classes) => {
-        return (
-          <div className='m-b-s'>
-            <button className={classes} disabled={isDisabled}>
-              {classes.split().map((c) => {
-                if (c.includes('loading')) {
-                  return <div class='button--loading__spinner'>Loading...</div>
-                } else if (icon) {
-                  return (
-                    <>
-                      <i className={`fas fa-${icon}`} />
-                      {!isIconOnly && <span>{label || `.${c}`}</span>}
-                    </>
-                  )
-                } else {
-                  return label || `.${c}`
-                }
-              })}
-            </button>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      {buttons.map(button => (
+        <div className='m-b-s'>
+          <button className={`${button.classes} ${extra_classes}`} disabled={isDisabled}>
+            {isLoading &&
+            <div className='button--loading__spinner'>Loading...</div>}
+            {icon && <i className={`fas fa-${icon}`} />}
+            {!isIconOnly && <span>{text('Button text', '') || button.label}</span>}
+          </button>
+        </div>
+      ))}
+    </>
   )
 }
 
 export const button_large = () =>
-  renderButtons([
-    'button button--large',
-    'button button--large button--secondary',
-    'button button--large button--tertiary',
-    'button button--large button--quaternary',
-  ])
+  renderButtons('button--large')
 
 export const button_medium = () =>
-  renderButtons([
-    'button',
-    'button button--secondary',
-    'button button--tertiary',
-    'button button--quaternary',
-  ])
+  renderButtons()
 
 export const button_small = () =>
-  renderButtons([
-    'button button--small',
-    'button button--small button--secondary',
-    'button button--small button--tertiary',
-    'button button--small button--quaternary',
-  ])
+  renderButtons('button--small')
 
 export const button_large_icon = () =>
-  renderButtons([
-    'button button--large button--icon',
-    'button button--large button--secondary button--icon',
-    'button button--large button--tertiary button--icon',
-    'button button--large button--quaternary button--icon',
-  ])
+  renderButtons('button--large button--icon')
 
 export const button_medium_icon = () =>
-  renderButtons([
-    'button button--icon',
-    'button button--secondary button--icon',
-    'button button--tertiary button--icon',
-    'button button--quaternary button--icon',
-  ])
+  renderButtons('button--icon')
 
 export const button_small_icon = () =>
-  renderButtons([
-    'button button--small button--icon',
-    'button button--small button--secondary button--icon',
-    'button button--small button--tertiary button--icon',
-    'button button--small button--quaternary button--icon',
-  ])
+  renderButtons('button--small button--icon')
 
 export const button_disabled = () =>
-  renderButtons(
-    [
-      'button',
-      'button button--secondary',
-      'button button--tertiary',
-      'button button--quaternary',
-    ],
-    true,
-  )
+  renderButtons('', true)
 
 export const button_loading = () =>
-  renderButtons([
-    'button button--small button--loading',
-    'button button--small button--loading button--secondary',
-    'button button--small button--loading button--tertiary',
-    'button button--small button--loading button--quaternary',
-    'button button--loading',
-    'button button--loading button--secondary',
-    'button button--loading button--tertiary',
-    'button button--loading button--quaternary',
-    'button button--large button--loading',
-    'button button--large button--loading button--secondary',
-    'button button--large button--loading button--tertiary',
-    'button button--large button--loading button--quaternary',
-  ])
-
-export const button_small_only_icon = () =>
-  renderButtons(
-    [
-      'button button--small button--only-icon',
-      'button button--small button--only-icon button--secondary',
-      'button button--small button--only-icon button--tertiary',
-      'button button--small button--only-icon button--quaternary',
-      'button button--small button--only-icon button--loading',
-      'button button--small button--only-icon button--loading button--secondary',
-      'button button--small button--only-icon button--loading button--tertiary',
-      'button button--small button--only-icon button--loading button--quaternary',
-    ],
-    false,
-    true,
-  )
-
-export const button_medium_only_icon = () =>
-  renderButtons(
-    [
-      'button button--only-icon',
-      'button button--only-icon button--secondary',
-      'button button--only-icon button--tertiary',
-      'button button--only-icon button--quaternary',
-      'button button--only-icon button--loading',
-      'button button--only-icon button--loading button--secondary',
-      'button button--only-icon button--loading button--tertiary',
-      'button button--only-icon button--loading button--quaternary',
-    ],
-    false,
-    true,
-  )
+  renderButtons('button--loading')
 
 export const button_large_only_icon = () =>
-  renderButtons(
-    [
-      'button button--large button--only-icon',
-      'button button--large button--only-icon button--secondary',
-      'button button--large button--only-icon button--tertiary',
-      'button button--large button--only-icon button--quaternary',
-      'button button--large button--only-icon button--loading',
-      'button button--large button--only-icon button--loading button--secondary',
-      'button button--large button--only-icon button--loading button--tertiary',
-      'button button--large button--only-icon button--loading button--quaternary',
-    ],
-    false,
-    true,
-  )
+  renderButtons('button--large button--only-icon')
+
+export const button_medium_only_icon = () =>
+  renderButtons('button--only-icon')
+
+export const button_small_only_icon = () =>
+  renderButtons('button--small button--only-icon')
 
 export const button_full_width = () =>
-  renderButtons([
-    'button button--full-width',
-    'button button--full-width button--secondary',
-    'button button--full-width button--tertiary',
-    'button button--full-width button--quaternary',
-  ])
+  renderButtons('button--full-width')
 
 export const button_auto_width = () =>
-  renderButtons([
-    'button button--auto-width',
-    'button button--auto-width button--secondary',
-    'button button--auto-width button--tertiary',
-    'button button--auto-width button--quaternary',
-  ])
+  renderButtons('button--auto-width')
 
 export const button_tiny_toggle = () => (
   <button className='button button--tiny-toggle' type='button'>
