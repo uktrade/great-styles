ebbd5adfbdabb6771c6c6092da9b3f2149b08ae7
Fred Marecesche <fred@froddd.co.uk>
2022-02-17 14:30:03 +0000

    Tidy up horizontal rule

--- src/stories/to-be-sorted/Linerule.stories.js
+++ src/stories/to-be-sorted/Linerule.stories.js
@@ -1,14 +1,18 @@
 import React from 'react'
 
-export default { title: 'To be sorted/Elements/Linerule' }
+export default { title: 'Elements/Horizontal rule' }
 
 export const Light = () => (
-  <div className='m-s width-full'>
+  <div className='m-s body-l'>
+    <p>Lorem ipsum dolor sit amet.</p>
     <hr className='hr hr--light' />
+    <p>Lorem ipsum dolor sit amet.</p>
   </div>
 )
 export const Dark = () => (
-  <div className='m-s width-full'>
+  <div className='m-s body-l'>
+    <p>Lorem ipsum dolor sit amet.</p>
     <hr className='hr hr--dark' />
+    <p>Lorem ipsum dolor sit amet.</p>
   </div>
 )
