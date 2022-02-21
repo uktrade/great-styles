f4ca4a2087e63a2a11ec16ce90a9b15a5ec8e7af
Fred Marecesche <fred@froddd.co.uk>
2022-02-21 11:58:13 +0000

    Move Learn Linking 'card'

--- src/stories/components/Card.stories.js
+++ src/stories/components/Card.stories.js
@@ -133,3 +133,20 @@ export const NextStep = () => (
     </div>
   </div>
 )
+
+// FIXME: this isn't even a card, it uses base styles all round?!
+export const LearnLinking = () => (
+  <a className='text-white link inline-block m-r-m' href='#'>
+    <h3 className='text-blue-deep-80 body-l-b m-b-xs'>Learn</h3>
+    <div className='bg-blue-deep-80 p-s radius'>
+      <h4 className='body-m text-white'>About to your Business</h4>
+      <p className='text-white h-s m-t-0'>
+        Dove Gin was founded in 2012 when Simon Dove started.
+      </p>
+      <div className='text-white'>
+        <i className='fa fa-clock text-blue-deep-40 m-r-xs' />
+        <span className='body-m text-white'>2 min read</span>
+      </div>
+    </div>
+  </a>
+)
