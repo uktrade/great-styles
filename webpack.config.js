42763f9aa31ba739b16122e15d64dce16644ac91
Fred Marecesche <fred@froddd.co.uk>
2022-02-22 16:22:31 +0000

    Fix last dependencies vulnerabilities

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const RemovePlugin = require('remove-files-webpack-plugin')
 
@@ -16,16 +15,12 @@ module.exports = {
     rules: [
       {
         test: /\.s?css$/i,
+        exclude: /node_modules/,
+        type: 'asset/source',
+        generator: {
+          filename: '[name].css',
+        },
         use: [
-          // extract to separate file
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
           // Compiles Sass to CSS
           {
             loader: 'sass-loader',
@@ -35,7 +30,7 @@ module.exports = {
               sassOptions: {
                 outputStyle: 'compressed',
                 includePaths: [path.resolve(__dirname, 'src/scss')],
-              }
+              },
             },
           },
         ],
@@ -46,8 +41,8 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 0,
-            name: './images/[name].[ext]'
-          }
+            name: './images/[name].[ext]',
+          },
         },
       },
       {
@@ -56,20 +51,17 @@ module.exports = {
           loader: 'url-loader',
           options: {
             limit: 0,
-            name: './fonts/[name].[ext]'
-          }
-        }
+            name: './fonts/[name].[ext]',
+          },
+        },
       },
     ],
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
     new CopyPlugin({
       patterns: [
-        { from: "./src/scss/images", to: "images" },
-        { from: "./src/scss/fonts", to: "fonts" },
+        { from: './src/scss/images', to: 'images' },
+        { from: './src/scss/fonts', to: 'fonts' },
       ],
     }),
     new RemovePlugin({
@@ -82,5 +74,5 @@ module.exports = {
         ],
       },
     }),
-  ]
+  ],
 }
