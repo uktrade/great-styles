ae1b29eb847e12bbf4c7094111e9741beb147731
Fred Marecesche <fred@froddd.co.uk>
2022-02-22 10:03:45 +0000

    Implement small/large radio buttons

--- src/stories/forms/Radio.stories.js
+++ src/stories/forms/Radio.stories.js
@@ -2,13 +2,26 @@ import React from 'react'
 
 export default { title: 'Forms/Radio' }
 
-export const Radio = () => (
+export const Normal = () => (
   <ul>
     <li className='multiple-choice'>
       <input type='radio' id='radio-1' name='option' value='option 1' />
       <label htmlFor='radio-1'>Option 1</label>
     </li>
     <li className='multiple-choice'>
+      <input type='radio' id='radio-2' name='option' value='option 2' />
+      <label htmlFor='radio-2'>Option 2</label>
+    </li>
+  </ul>
+)
+
+export const Large = () => (
+  <ul>
+    <li className='multiple-choice large'>
+      <input type='radio' id='radio-1' name='option' value='option 1' />
+      <label htmlFor='radio-1'>Option 1</label>
+    </li>
+    <li className='multiple-choice large'>
       <input type='radio' id='radio-2' name='option' value='option 2' />
       <label htmlFor='radio-2'>Option 2</label>
     </li>
